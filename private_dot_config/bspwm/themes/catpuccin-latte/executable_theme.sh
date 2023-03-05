b0642cf09dbb1e68cb8c4b54cbaed8e3af375157
Hilan Meyran <hilanmeyran@protonmail.com>
2023-03-05 19:28:26 +0000

    initial desktop commit

---
+++ private_dot_config/bspwm/themes/catpuccin-latte/executable_theme.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+source ./colors.sh
+
+
+## Directories ------------------------------
+PATH_CONF="$HOME/.config"
+PATH_BSPWM="$PATH_CONF/bspwm"
+PATH_TERM="$PATH_BSPWM/alacritty"
+PATH_HELIX="$PATH_BSPWM/helix"
+PATH_PBAR="$PATH_BSPWM/polybar"
+PATH_ROFI="$PATH_BSPWM/rofi"
+
+
+# ------------------------------------------
+
+wallpaper="$PATH_CONF/wallpaper/day.webp"
+
+# Theme name
+theme="catppuccin-latte"
+
+apply_wallpaper() {
+	feh --bg-scale "$wallpaper"
+}
+
+apply_terminal(){
+	sed -i '/import:/{n; s#^.*$#  - '"$PATH_TERM"'/themes/catppuccin-latte.yml#}' "$PATH_TERM/alacritty.yml"
+}
+
+apply_helix(){
+	sed -i 's#theme =.*#theme = \"'"catppuccin_latte"'\"#' "$PATH_HELIX/config.toml"
+}
+
+# Can't pinpoint good regex, so just changes the content of the already included colors.ini
+apply_polybar(){
+	cat "$PATH_PBAR/themes/cattpuccin-latte.ini" > "$PATH_PBAR/colors.ini"
+}
+
+apply_wallpaper
+apply_terminal
+apply_helix
+apply_polybar
