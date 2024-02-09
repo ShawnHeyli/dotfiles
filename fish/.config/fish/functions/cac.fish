645b65b162196a6b017140c317d7099df5c47f04
Hilan Meyran <hilanmeyran@protonmail.fr>
2024-02-09 12:27:39 +0100

    fish cat alias

---
+++ fish/.config/fish/functions/cac.fish
@@ -0,0 +1,4 @@
+function cac --wraps='wl-copy <' --description 'alias cac=wl-copy <'
+  wl-copy < $argv
+        
+end
--- fish/.config/fish/functions/cap.fish
+++ fish/.config/fish/functions/cap.fish
@@ -1,4 +1,4 @@
-function cap --wraps=bat\ --paging=never\ --style=\'plain\' --wraps=wl-copy --wraps=wl-copy\ \&\&\ echo\ \'Copied\ to\ clipboard\ !\' --wraps='wl-copy <' --description 'alias cap=wl-copy <'
-  wl-copy < $argv
+function cap --wraps='bat -p' --description 'alias cap=bat -p'
+  bat -p $argv
         
 end
