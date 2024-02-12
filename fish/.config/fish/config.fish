d7f9d3e3c4f9c58c93ecada7a2a17f7c2f6e6537
Hilan Meyran <hilanmeyran@protonmail.fr>
2024-02-12 10:36:25 +0100

    ssh-agent for gitui

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,6 @@
+if [ 0 -eq $(pgrep -c ssh-agent) ]
+  eval (ssh-agent -c) > /dev/null
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
 starship init fish | source
