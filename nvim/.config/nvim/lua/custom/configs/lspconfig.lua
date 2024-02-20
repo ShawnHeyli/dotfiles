4867c50c41b6844dc8bcd53bdde3d8251af976d0
Hilan Meyran <hilanmeyran@protonmail.fr>
2024-02-20 08:40:21 +0100

    clangd fix

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,27 +1,13 @@
 local base = require("plugins.configs.lspconfig")
 local on_attach = base.on_attach
 local capabilities = base.capabilities
-local utils = require "lspconfig/util"
 local lspconfig = require("lspconfig")
 
 lspconfig.clangd.setup {
-  on_attach = function(client, bufnr)
-    client.server_capabilities.signatureHelpProvider = false
-    on_attach(client, bufnr)
-  end,
+  on_attach = on_attach,
   capabilities = capabilities,
+  cmd = {
+    "clangd",
+    "-offset-encoding=utf-16",
+  }
 }
-
--- lspconfig.rust_analyzer.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   filetypes = { "rust" },
---   root_dir = utils.root_pattern("Cargo.toml", "rust-project.json"),
---   settings = {
---     ["rust-analyzer"] = {
---       cargo = {
---         allFeatures = true,
---       },
---     },
---   },
--- }
